9c02febfdb29c985738a5d7b9fd0b80c91127ce3
sakib <nsakib.cse@gmail.com>
2021-09-15 21:39:09 +0600

    Refactor: Optimize code for single file upload.

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const multer  = require('multer')
-const upload = multer({ dest: process.env.FOLDER })
+const upload = multer({ dest: process.env.FOLDER }).single('file');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/files', upload.single('file'), function (req, res) {
+router.post('/files', upload, function (req, res) {
   res.status(200).send();
 });
 
