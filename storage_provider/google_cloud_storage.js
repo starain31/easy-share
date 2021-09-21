173489a436c76ff35e4e0ec6af9335c291d60549
sakib <nsakib.cse@gmail.com>
2021-09-22 03:38:24 +0600

    Implement post file for google could storage.

---
+++ storage_provider/google_cloud_storage.js
@@ -0,0 +1,70 @@
+const {Storage} = require('@google-cloud/storage');
+const {randomUUID} = require("crypto");
+const path = require("path");
+const fs = require("fs");
+
+function create({db, CONFIG}) {
+    const {keyFilename, bucketName} = JSON.parse(fs.readFileSync(CONFIG).toString());
+
+    const storage = new Storage({keyFilename});
+
+    async function post({file}) {
+        const publicKey = `public-${randomUUID()}`;
+        const privateKey = `private-${randomUUID()}`;
+
+        const file_path = path.join(`${publicKey}_${file.originalname}`);
+
+        await storage.bucket(bucketName).file(file_path).save(file.buffer);
+
+        await db.save_keys({
+            publicKey,
+            privateKey,
+            value: {
+                ...file,
+                buffer: undefined,
+                path: file_path,
+                publicKey,
+                privateKey
+            }
+        });
+        return {publicKey, privateKey};
+    }
+
+    async function get({publicKey}) {
+        //TODO:
+        //Implement...
+
+        // const file_details = await db.get_value_by_key({key: publicKey});
+        //
+        // if (!file_details) {
+        //     throw "No such file";
+        // }
+        //
+        // const content = fs.readFileSync(file_details.path);
+        //
+        // return {content, file_details}
+    }
+
+    async function del({privateKey}) {
+        //TODO:
+        //Implement...
+
+        // const file_details = await db.get_value_by_key({key: privateKey});
+        //
+        // if (!file_details || !file_details.path || !fs.existsSync(file_details.path)) {
+        //     throw 'No such file';
+        // }
+        //
+        // fs.unlinkSync(file_details.path);
+        // await db.delete_by_key({key: privateKey});
+    }
+
+    return {
+        post,
+        get,
+        del
+    };
+}
+
+module.exports = create;
+
--- storage_provider/index.js
+++ storage_provider/index.js
@@ -1,14 +1,19 @@
-const local = require("./local_file_system_storage");
+const local_file_system_storage = require("./local_file_system_storage");
+const google_cloud_storage  = require('./google_cloud_storage');
 
 const PROVIDERS = {
     GOOGLE: 'google',
     LOCAL: 'local'
 }
 
-function storage_provider({PROVIDER, db, FOLDER}) {
+function storage_provider({PROVIDER, db, FOLDER, CONFIG}) {
     switch (PROVIDER) {
+        case PROVIDERS.GOOGLE:
+            return google_cloud_storage({db, CONFIG});
+
         case PROVIDERS.LOCAL:
-            return local({db, FOLDER});
+        default:
+            return local_file_system_storage({db, FOLDER});
     }
 }
 
